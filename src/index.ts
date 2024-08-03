58c5144fbac8a10beb910aa43b86ebb433671aa6
Tomáš Hvozdovič <me@menox.dev>
2024-08-03 13:27:16 +0200

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from './app';
 import dotenv from 'dotenv';
 import { mongoConnect } from './config/mongo.connect';
+import { startCronJobs } from './cron';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ mongoConnect(process.env.MONGO_URI!);
 
 // Port:
 const PORT = process.env.PORT || 5000;
+
+startCronJobs();
 
 // Listen:
 const server = app.listen(PORT, () =>
